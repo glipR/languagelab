97c50b3fb47437f841439630cf92d244aebcbf2c
Jackson Goerner <jgoerner@squareup.com>
2024-11-24 11:33:12 +1100

    Have terminal clear button always float in top-right.

--- templates/terminal.js
+++ templates/terminal.js
@@ -3,6 +3,10 @@ export function addTerminal() {
   terminal.classList.add("terminal");
   terminal.id = "terminal";
   terminal.style.display = "none";
+  const terminalLineContainer = document.createElement("div");
+  terminalLineContainer.classList.add("terminal-line-container");
+  terminalLineContainer.id = "terminal-line-container";
+  terminal.appendChild(terminalLineContainer);
   const clear = document.createElement("button");
   clear.classList.add("terminal-clear");
   clear.addEventListener("click", clearTerminal);
@@ -16,13 +20,13 @@ export function newLog(method='log') {
   return function message(...args) {
     if (method === 'error') oldError(...args);
     if (method === 'log') oldLog(...args);
-    const terminal = document.getElementById("terminal");
+    const terminal = document.getElementById("terminal-line-container");
     const log = document.createElement("div");
     log.classList.add("terminal-line");
     log.classList.add(method);
     log.innerHTML = args.join(" ").replaceAll("\n", "<br>");
     terminal.appendChild(log);
-    terminal.style.display = "block";
+    document.getElementById("terminal").style.display = "block";
   }
 }
 
